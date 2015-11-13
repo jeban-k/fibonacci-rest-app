62f3895c41a9231d23fad163b11d9dbe3524eb02
Jeban K <jebanephrim@gmail.com>
2015-11-13 12:57:33 -0500

    functionality done, tests to be added

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+def fibo(n):
+  list1=[0]
+  f1,f2 = 0,1
+  for i in range(n-1):
+    f1,f2 = f2,f1+f2
+    list1.append(f1)
+  return list1
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,10 +18,27 @@ class HelloWorld(Resource):
 
 class FibonacciGet(Resource):
     def get(self, num):
-        return {'number':num}
+        if num.isnumeric():          
+          num = int(num)
+          if num < 0:
+            return {'Exception' : 'please enter positive values' }
+          elif num == 0:
+            return {'fibonacci' : '0' }
+          elif num == 1:
+            return {'fibonacci':'0,1'}
+          else :
+            return {'fibonacci':fibo(num) }
+        else:
+          return {'Exception' : 'Please enter positive numeric values' }
+
+class FibonacciHandler(Resource):
+     def get(self):
+        return {'Exception' : 'Enter any numeric value to get desired result' }
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(FibonacciGet, '/fibonacci/get/<string:num>')
+api.add_resource(FibonacciHandler, '/fibonacci/get/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
